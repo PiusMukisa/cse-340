d9c6c93cd28fdf61b72ce1736df41185d11eca74
PiusMukisa <mukisapius2004@gmail.com>
2025-04-09 11:04:40 +0300

    WEEK-3

---
+++ models/inventory-model.js
@@ -0,0 +1,42 @@
+const pool = require("../database")
+
+const inventoryModel = {}
+
+/* ***************************
+ *  Get all classification data
+ * ************************** */
+inventoryModel.getClassifications = async function () {
+  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+}
+
+/* ***************************
+ *  Get all inventory items and classification_name by classification_id
+ * ************************** */
+inventoryModel.getInventoryByClassificationId = async function (classification_id) {
+  try {
+    const data = await pool.query(
+      `SELECT * FROM public.inventory AS i 
+          JOIN public.classification AS c 
+          ON i.classification_id = c.classification_id 
+          WHERE i.classification_id = $1`,
+      [classification_id]
+    )
+    return data.rows
+  } catch (error) {
+    console.error("getclassificationsbyid error " + error)
+  }
+}
+
+inventoryModel.getInventoryById = async function (invId) {
+  try {
+    const data = await pool.query(
+      "SELECT * FROM public.inventory AS i JOIN public.classification AS c ON i.classification_id = c.classification_id WHERE i.inv_id = $1",
+      [invId]
+    )
+    return data.rows[0]
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+module.exports = inventoryModel
